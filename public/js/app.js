48cb7b592d38e442041f0b6e19bae652a746746b
kmilo9713 <kmilo9713.cl@gmail.com>
2020-07-04 14:44:50 -0500

    learning new stuff

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/kmilo9713/Programacion/Web/Laravel/MyHobbies/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/kmilo9713/Programacion/Web/Laravel/MyHobbies/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/kmilo9713/Programacion/git_projects/MyHobbies---Laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/kmilo9713/Programacion/git_projects/MyHobbies---Laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem ullam nesciunt maxime totam dolorum delectus! Culpa consequuntur porro provident, expedita, sunt, inventore explicabo facere fugit dicta alias placeat ab impedit.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
